f7152ef976999f5ffd9eb92b09793918e04add43
Robeco945 <carettoroberto0312@gmail.com>
2026-02-02 20:32:26 +0200

    done w3

---
+++ Junit_w3/src/main/java/TemperatureConverter.java
@@ -0,0 +1,12 @@
+public class TemperatureConverter {
+    public static double fahrenheitToCelsius (double a){
+        return((a - 32)*5);
+    }
+    public static double celsiusToFahrenheit (double a){
+        return(a*9/5+32);
+    }
+    public static boolean isExtremeTemperature (double a){
+        return (a < -40 || a > 50);
+        }
+    }
+
---
+++ Junit_w3/src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    void testConversions() {
+        assertEquals(0.0, converter.fahrenheitToCelsius(32.0), 0.1);
+        assertEquals(32.0, converter.celsiusToFahrenheit(0.0), 0.1);
+    }
+
+    @Test
+    void testExtreme() {
+        assertTrue(converter.isExtremeTemperature(-45));
+        assertFalse(converter.isExtremeTemperature(25));
+    }
+}
